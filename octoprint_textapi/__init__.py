c9fba575454822ea7579bca3e34048493d4bba61
Stephen Berry <sberry@gravity-performance.com>
2022-01-12 09:50:01 -0500

    changes to the API for a more generic email/text interface. Basic testing done.

--- octoprint_textapi/__init__.py
+++ octoprint_textapi/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from email.message import EmailMessage
+from email.utils import formatdate
+
 import flask
 import octoprint.plugin
 
@@ -34,73 +37,57 @@ class TextapiPlugin(
 
     # for this test a button is included in the nav bar that initiates the sending of the message (empty mailbox)
     def on_api_get(self, request):
+        """
+        Format an EmailMessage to send to OctoText
+        """
 
         self._logger.debug("TEST API The test button was pressed...")
         self._logger.debug(f"request = {request}")
 
-        title = "This is the message title"  # text only, no special characters
-        description = "this is the body of the message or email"
-        printer_name = (
-            self._identifier  # you can use this to inform people this is coming from your plugin
-        )
-        filename = (
-            # self._basefolder + "/static/img/Alert.png"
-            self._basefolder
-            + "/static/img/Alert.zip"
+        backupFileLocation = (
+            self._basefolder + "/static/img/Alert.zip"
         )  # path to an image or file to be sent. Set to None if not used.
-        # filename = None
-        # ftype = "jpg"
-        ftype = "zip"
 
-        #  atype = (
-        #    "image"  # see EmailMessage.add_attachment() for types; application for zip.
-        # )
-        atype = "application"  # see EmailMessage.add_attachment() for types; application for zip.
-        do_cam_snapshot = (
-            True  # True tries to send an image from the webcam if enabled in OctoText
-            # only one file is sent, either the specified file or webcam and the
-            # file takes precedence
-        )
-        data = dict(
-            [
-                ("title", title),
-                ("description", description),
-                ("sender", printer_name),
-                ("filename", filename),
-                ("filetype", ftype),
-                ("attachment_type", atype),
-                ("send_image", do_cam_snapshot),
-            ]
+        emailMessage = EmailMessage()
+        emailMessage["Subject"] = "Test from API"
+        # changing the From or To settings in an email message will override the Octotext settings
+        # this may or may not work depending on your mail provider. Leaving these fields blank
+        # uses the user settings in Octotext
+        # emailMessage["From"] = "OctoText@outlook.com"  # 'OctoText@outlook.com'
+        # emailMessage["To"] = "ollisgit+octotext@gmail.com"
+        emailMessage["Date"] = formatdate(localtime=True)
+        emailMessage.set_content("This is my body message", charset="utf-8")
+
+        fp = open(backupFileLocation, "rb")
+        emailMessage.add_attachment(
+            fp.read(),
+            maintype="application",
+            subtype="zip",
+            filename="myBackupFilename.zip",
         )
 
-        # using the helper function to call OctoText. send_text assigned in on_after_startup()
-        if self.send_text:
-            result = self.send_text(command="OctoText", data={"test": data})
-            self._logger.debug(f"OctoText api result {result}")
-            return flask.make_response(flask.jsonify(result=True, error=result))
-
-        # the following code sends the message through the PluginManager and is less efficient than using the helper
-        # above. As written this code will never execute as OctoText has a helper function and it is the preferred way
-        # to access the plugin.
-        # It does work however, and it is left here because the code functions and may be helpful in some cases.
+        # send the email using OctoText
+        self.send_email(command="OctoText", data=emailMessage)
+        return flask.make_response(flask.jsonify(result=True, error=None))
 
         # check to see if OctoText exists
-        p_info = self._plugin_manager.get_plugin_info("OctoText", require_enabled=True)
-        if p_info is None:
-            self._logger.debug("OctoText is not loaded or enabled on this system!")
-            error = "NOT_LOADED"
-            return flask.make_response(flask.jsonify(result=True, error=error))
-        self._logger.debug(f"OctoText version {p_info.version}")
-        self._logger.debug(f"OctoText info block: {p_info}")
-        if p_info.loaded:
-            self._logger.debug("OctoText has been loaded")
-        error = None
-        try:
-            self._plugin_manager.send_plugin_message("OctoText", {"test": data})
-        except Exception as e:
-            error = "NOT_ENABLED"
-            self._logger.debug(f"Exception sending API message: {e}")
-        return flask.make_response(flask.jsonify(result=True, error=error))
+        # p_info = self._plugin_manager.get_plugin_info("OctoText", require_enabled=True)
+        # if p_info is None:
+        #     self._logger.debug("OctoText is not loaded or enabled on this system!")
+        #     error = "NOT_LOADED"
+        #     return flask.make_response(flask.jsonify(result=True, error=error))
+        # self._logger.debug(f"OctoText version {p_info.version}")
+        # self._logger.debug(f"OctoText info block: {p_info}")
+        # if p_info.loaded:
+        #     self._logger.debug("OctoText has been loaded")
+        # error = None
+
+        # try:
+        #     self._plugin_manager.send_plugin_message("OctoText", {"test": data})
+        # except Exception as e:
+        #     error = "NOT_ENABLED"
+        #     self._logger.debug(f"Exception sending API message: {e}")
+        # return flask.make_response(flask.jsonify(result=True, error=error))
 
     sent_text = ""
 
@@ -116,8 +103,8 @@ class TextapiPlugin(
         #              data: {__getitem__},
         #              permissions: Any = None) -> Optional[bool]
         helpers = self._plugin_manager.get_helpers("OctoText")
-        if helpers and "send_text" in helpers:
-            self.send_text = helpers["send_text"]
+        if helpers and "send_email" in helpers:
+            self.send_email = helpers["send_email"]
 
     ##~~ Softwareupdate hook
 
